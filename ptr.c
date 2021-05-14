ce4b460b5c6c8d7eca0f7dc28960d7444475fbdf
abhay <abhaytaras18@gmail.com>
2021-05-14 19:12:09 +0530

    xs

--- ptr.c
+++ ptr.c
@@ -1,33 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h> // header file
-
-#define MAX 100 // preprocessor 
-
-struct id // structure 
-{
-    int no;
-    char name[100];
-} person; // accessing var
-
-int main()
-{
-
-int  *ptr , x; 
-ptr = &x;
-x = 10;
-printf("enter the id no => ");
-scanf("%d",&person.no);
-printf("\nenter name => ");
-scanf("%s",&person.name);
-
-printf("%d\n%d\n%d\n%d\n%d\n",x,ptr,&ptr,*ptr);
-
-printf("\nthe id no is = %d\nname is %s",person.no,person.name);
-
-return 0;
-
-
-}
-
+ #include<stdio.h>
+ int main()
+ {
+   int i = 10;
+   for ( i = 0; i < 10; i++)
+   {
+      printf("hello");
     
+   }
+    return 0;
