9ab5b4646684341d2e599a7e28a2415eee1b55b2
abhay <abhaytaras18@gmail.com>
2021-05-15 19:55:20 +0530

    do

--- ptr.c
+++ ptr.c
@@ -1,10 +1,20 @@
+ #include<stdlib.h>
  #include<stdio.h>
  int main()
  {
-   int i = 10;
-   for ( i = 0; i < 10; i++)
+   int *a, n,i;
+
+   printf("Enter no of element > : ");
+   scanf("%d",&n);
+
+   a=(int*)malloc(n*sizeof(int));
+
+   for (i = 0; i < n; i++)
    {
-      printf("hello");
-    
+         printf("enter the elements > : ");
+         scanf("%d",&a);
+         printf("\n%d",a);
    }
+   
     return 0;
+ }
